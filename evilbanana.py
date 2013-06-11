6dd74afa66e4247b505f7af016db50bcd24639ff
Falling Duck <handcrafteddoom@gmail.com>
2013-06-10 16:45:45 -0800

    Per OrcaCat's request, implemented sprites. All code should produce the same results as before.

--- evilbanana.py
+++ evilbanana.py
@@ -1,63 +1,48 @@
-''' Cat platformer canvas V 0.2 by OrcaCat and GraviPy
+''' Cat platformer canvas V 0.2 by OrcaCat, GraviPy, and Falling Duck
     For Evil Banana project '''
 
 
-# Follow PEP8 people!
-
-
 import pygame
-import sys
-#from pygame.locals import *
+#import sys
 import random
 
 
-class BananaEntity(object):
+class BananaEntity(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, health, xvel, yvel, gPixDat):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, health, xVel, yVel, gPixDat):
+        pygame.sprite.Sprite.__init__(self)
         self.health = health
-        self.xvel = xvel
-        self.yvel = yvel
-        self.gPixDat = gPixDat
+        self.xVel = xVel
+        self.yVel = yVel
 
-    def renderOnScreen(self, screen):
-        screen.blit(self.gPixDat, (self.x, self.y))
+        self.image = gPixDat
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-    def doTick(self):
-        self.xvel *= 0.9
-        self.yvel *= 0.9
-        self.yvel += 4
-        self.x += self.xvel
-        self.y += self.yvel
-        if self.y > 200:
-            self.y = 200
-            self.yvel = 0
+    def _tick(self):
+        self.xVel *= 0.9
+        self.yVel *= 0.9
+        self.yVel += 4
+        self.rect.x += self.xVel
+        self.rect.y += self.yVel
+        if self.rect.y > 200:
+            self.rect.y = 200
+            self.yVel = 0
         if self.x > 270:
-            self.x = 270
+            self.rect.x = 270
         if self.x < 0:
-            self.x = 0
+            self.rect.x = 0
 
 
 class Player(BananaEntity):
 
-    def __init__(self, x, y, health, xvel, yvel, gPixDat):
-        BananaEntity.__init__(self, x, y, health, xvel, yvel, gPixDat)
-        self.arrowsPressed = [False, False, False, False]
+    def __init__(self, x, y, health, xVel, yVel, gPixDat):
+        BananaEntity.__init__(self, x, y, health, xVel, yVel, gPixDat)
+        self.arrowsPressed = [False] * 4
 
-    def doTick(self):
-        self.xvel *= 0.9
-        self.yvel *= 0.9
-        self.yvel += 4
-        self.x += self.xvel
-        self.y += self.yvel
-        if self.y > 200:
-            self.y = 200
-            self.yvel = 0
-        if self.x > 270:
-            self.x = 270
-        if self.x < 0:
-            self.x = 0
+    def tick(self):
+        self._tick()
         if self.arrowsPressed[0]:  # If up arrow is pressed
             if self.y >= 200:
                 self.yvel = -40
@@ -69,12 +54,12 @@ class Player(BananaEntity):
 
 class Enemy(BananaEntity):
 
-    def __init__(self, x, y, health, xvel, yvel, gPixDat):
-        BananaEntity.__init__(self, x, y, health, xvel, yvel, gPixDat)
+    def __init__(self, x, y, health, xVel, yVel, gPixDat):
+        BananaEntity.__init__(self, x, y, health, xVel, yVel, gPixDat)
 
 
-def newEnemy(x, y, health, xvel, yvel, gPixDat, group):
-    group.append(Enemy(x, y, health, xvel, yvel, gPixDat))
+def newEnemy(x, y, health, xVel, yVel, gPixDat, group):
+    group.append(Enemy(x, y, health, xVel, yVel, gPixDat))
 
 
 pygame.init()
@@ -83,33 +68,39 @@ fpsClock = pygame.time.Clock()
 DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
 pygame.display.set_caption('Platformer Canvas')
 playerimg = pygame.image.load('banana.png')
+goodBananas = pygame.sprite.Group()
 protoBanana = Player(10, 10, 10, 0, 0, playerimg)
-badBananas = []
-while True:
+goodBananas.add(protoBanana)
+badBananas = pygame.sprite.Group()
+running = True
+while running:
     DISPLAYSURF.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
+            continue
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                continue
+            elif event.key == pygame.K_UP or event.key == pygame.K_w:
                 protoBanana.arrowsPressed[0] = True
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 protoBanana.arrowsPressed[3] = True
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+            elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
                 protoBanana.arrowsPressed[2] = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_w:
                 protoBanana.arrowsPressed[0] = False
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 protoBanana.arrowsPressed[3] = False
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+            elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
                 protoBanana.arrowsPressed[2] = False
-    protoBanana.doTick()
-    protoBanana.renderOnScreen(DISPLAYSURF)
+    protoBanana.tick()
+    goodBananas.draw(DISPLAYSURF)
     for i in badBananas:
-        i.doTick()
-        i.renderOnScreen(DISPLAYSURF)
+        i._tick()
+    badBananas.draw(DISPLAYSURF)
     pygame.display.update()
     fpsClock.tick(FPS)
 pygame.quit()
